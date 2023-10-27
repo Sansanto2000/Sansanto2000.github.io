b95ef90bbb1796277e99986126c472ec7b6ab9d2
Santiago Ponte <64858429+Sansanto2000@users.noreply.github.com>
2023-10-27 12:07:02 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://Sansanto2000.github.io',
+  base: '/Sansanto2000.github.io',
+})
