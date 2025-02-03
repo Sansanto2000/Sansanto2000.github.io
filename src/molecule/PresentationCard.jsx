ca77cbe8d313b7ab1747f066c96567e80b960951
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2025-02-03 20:14:21 -0300

    :construction: formateo preeliminar

--- src/molecule/PresentationCard.jsx
+++ src/molecule/PresentationCard.jsx
@@ -1,10 +1,18 @@
-
+import "../styles.css";
 
 export default function PresentationCard() {
     return (
         <>
-            <h1>Santiago Ponte</h1>   
-            <img src="SP.jpg" alt="Foto de Santiago Ponte" width={200} />
+            <div className="top-card">
+                <img src="SP.jpg" alt="Foto de Santiago Ponte" />
+            </div>
+            <div className="bottom-card">
+                <h1>Santiago Andres Ponte Ahón</h1>   
+                <p>- Licenciado en Sistemas</p>   
+                <p>- Licenciado en Informática</p>   
+                <h2>Actualemente</h2>   
+                <p>- Becario Doctoral en el Intituto de Investigación Informática LIDI (III-LIDI) de la Universidad Nacional de La Plata (UNLP)</p>   
+            </div>
         </>
     );
 }
